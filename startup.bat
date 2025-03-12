c5a6089a862c90d5e0c2dc427f5c93aeac844b33
sum0976 <sum0976@gmail.com>
2025-03-12 21:48:44 +0800

    Add bulma to Report Lost JS

---
+++ startup.bat
@@ -0,0 +1,13 @@
+cd D:\Project\Pet-Search-United\backend
+
+node index.js
+
+cd D:\Project\Pet-Search-United\frontend
+
+npm start
+
+cd D:\Project\Pet-Search-United\ml
+
+venv\Scripts\activate
+
+python app.py
